067e3c5706fe6e9c99df44e00e584b73744afb12
rreeggeenntt4 <rreeggeenntt4@yandex.ru>
2024-02-17 01:46:52 +0300

    ready

--- index.php
+++ index.php
@@ -11,24 +11,18 @@ function second_simvol($str)
     $max = 0;
     $second_max = 0;
     foreach (count_chars($str, 1) as $i => $val) {
-        echo "\"", chr($i), "\" встречается в строке $val раз(а).\n";
-        echo "<br><br>";
         $n_arr[chr($i)] = $val;
         if ($val > $max) {
             $second_max = $max;
             $max = $val;
         }
     }
-    echo "max = $max";
-    echo "<br>";
-    echo "second_max = $second_max";
-    echo "<br>";
+
     asort($n_arr, SORT_NUMERIC);
     print_r($n_arr);
-    echo "<br><br><br>";
 
-   return "Второй по встречаемости символ = '" . array_search($second_max, $n_arr) . "'";
+    return "Второй по встречаемости символ = '" . array_search($second_max, $n_arr) . "'";
 }
 
 $str = "abccdccefffgihhhhhj";
-second_simvol($str);
+echo second_simvol($str);
